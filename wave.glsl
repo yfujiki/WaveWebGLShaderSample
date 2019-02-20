ba776ea7c8ff3f0aaa5f44b018935d2eee4389ac
Yuichi Fujiki <yuichi@chaione.com>
2019-02-20 21:44:04 +0800

    Added high frequency elements to each wave.

--- wave.glsl
+++ wave.glsl
@@ -10,7 +10,9 @@ uniform vec2  r; // resolution
 float pi = 3.14 / 2.0;
 
 vec4 wave(vec4 src, vec2 position, float tt, vec4 color, float timeFactor, float offsetFactor, float level) {
-	float value = sin(-tt * timeFactor * 2.0 + (position.x + offsetFactor) * 2.0 * pi) * 0.2 + level * 0.25;
+	float value = sin(-tt * timeFactor * 2.0 + (position.x + offsetFactor) * 2.0 * pi) * 0.18;
+    value = value + sin(position.x * 4.0 * pi) * 0.02;
+    value = value + level * 0.25;
 	
 	if (position.y < value) {
 		return color;
@@ -25,9 +27,9 @@ void main(void){
 	vec4 src = vec4(0.0, 0.0, 0.0, 0.0);
 	
     vec4 dest1 = wave(src, p, t, vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0), 1.0, 0.0, 2.0);
-    vec4 dest2 = wave(dest1, p, t, vec4(66.0/255.0, 77.0/255.0, 83.0/255.0, 1.0), 1.2, 0.2, 1.0);
-    vec4 dest3 = wave(dest2, p, t, vec4(1.0/255.0, 0.0, 1.0/255.0, 1.0), 1.4, 0.4, 0.0);
-    vec4 dest4 = wave(dest3, p, t, vec4(0.0, 0.0, 0.0, 0.0), 1.2, 0.6, -2.0);
+    vec4 dest2 = wave(dest1, p, t, vec4(66.0/255.0, 77.0/255.0, 83.0/255.0, 1.0), 1.4, 0.4, 1.0);
+    vec4 dest3 = wave(dest2, p, t, vec4(1.0/255.0, 0.0, 1.0/255.0, 1.0), 1.8, 0.8, 0.0);
+    vec4 dest4 = wave(dest3, p, t, vec4(0.0, 0.0, 0.0, 0.0), 1.4, 0.4, -2.0);
     
     gl_FragColor = dest4;
 }
