044a848f28c33877f6bea833bd998afd8e3a4dee
Yuichi Fujiki <yuichi@chaione.com>
2019-02-21 10:11:16 +0800

    Adjusted to shadertoy format.

--- wave.glsl
+++ wave.glsl
@@ -1,7 +1,4 @@
 //Inspired by https://twitter.com/PJaYOfficial/status/1083459961341001728
-precision mediump float;
-uniform float t; // time
-uniform vec2  r; // resolution
 
 float pi = 3.14 / 2.0;
 
@@ -17,14 +14,15 @@ vec4 colorOfWave(float ttime, vec2 position, vec4 srcColor, vec4 targetColor, fl
 	return color;
 }
 
-void main(void){
-    vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
+void mainImage(out vec4 fragColor, in vec2 fragCoord ){
+    vec2 p = (fragCoord.xy * 2.0 - iResolution.xy) / min(iResolution.x, iResolution.y);
 
-	vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
-	vec4 color1 = colorOfWave(t, p, color, vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0), 1.0, 0.0, 0.8);
-	vec4 color2 = colorOfWave(t, p, color1, vec4(66.0/255.0, 77.0/255.0, 83.0/255.0, 1.0), 1.2, 0.4, 0.4);
-	vec4 color3 = colorOfWave(t, p, color2, vec4(1.0/255.0, 0.0, 1.0/255.0, 1.0), 1.4, 0.8, 0.0);
-	vec4 color4 = colorOfWave(t, p, color3, vec4(0.0, 0.0, 0.0, 0.0), 1.2, 0.4, -0.4);
+	vec4 color = vec4(1.0, 1.0, 1.0, 1.0);
+	vec4 color1 = colorOfWave(iTime, p, color, vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0), 1.0, 0.0, 0.8);
+	vec4 color2 = colorOfWave(iTime, p, color1, vec4(66.0/255.0, 77.0/255.0, 83.0/255.0, 1.0), 1.2, 0.4, 0.4);
+	vec4 color3 = colorOfWave(iTime, p, color2, vec4(1.0/255.0, 0.0, 1.0/255.0, 1.0), 1.4, 0.8, 0.0);
+	vec4 color4 = colorOfWave(iTime, p, color3, vec4(1.0, 1.0, 1.0, 1.0), 1.2, 0.4, -0.4);
 	
-    gl_FragColor = color4;
+    fragColor = color4;
 }
+
