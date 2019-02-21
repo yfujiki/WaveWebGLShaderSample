52660f748b9337f2141fbff6f0c8ddac79ed196c
Yuichi Fujiki <yuichi@chaione.com>
2019-02-21 09:29:58 +0800

    Just a sign wave

--- wave.glsl
+++ wave.glsl
@@ -8,12 +8,13 @@ float pi = 3.14 / 2.0;
 void main(void){
     vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
 
-	vec4 src = vec4(0.0, 0.0, 0.0, 0.0);
+	vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
 	
-    vec4 dest1 = wave(src, p, t, vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0), 1.0, 0.0, 2.0);
-    vec4 dest2 = wave(dest1, p, t, vec4(66.0/255.0, 77.0/255.0, 83.0/255.0, 1.0), 1.4, 0.4, 1.0);
-    vec4 dest3 = wave(dest2, p, t, vec4(1.0/255.0, 0.0, 1.0/255.0, 1.0), 1.8, 0.8, 0.0);
-    vec4 dest4 = wave(dest3, p, t, vec4(0.0, 0.0, 0.0, 0.0), 1.4, 0.4, -2.0);
+	float value = sin(p.x * 2.0 * pi);
+
+	if (p.y < value) {
+		color = vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0);
+	}
     
-    gl_FragColor = dest4;
+    gl_FragColor = color;
 }
