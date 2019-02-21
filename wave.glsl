cc9c27b82a0ffadceaadad25b7e67ff1b5d67cb8
Yuichi Fujiki <yuichi@chaione.com>
2019-02-21 09:40:07 +0800

    Created a function for wave

--- wave.glsl
+++ wave.glsl
@@ -5,16 +5,22 @@ uniform vec2  r; // resolution
 
 float pi = 3.14 / 2.0;
 
+vec4 colorOfWave(float ttime, vec2 position, vec4 srcColor, vec4 targetColor) {
+	float value = sin((ttime + position.x) * 2.0 * pi);
+
+	vec4 color = srcColor;
+	if (position.y < value) {
+		color = targetColor;
+	}
+	
+	return color;
+}
+
 void main(void){
     vec2 p = (gl_FragCoord.xy * 2.0 - r) / min(r.x, r.y);
 
 	vec4 color = vec4(0.0, 0.0, 0.0, 0.0);
+	vec4 destColor = colorOfWave(t, p, color, vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0));
 	
-	float value = sin((t + p.x) * 2.0 * pi);
-
-	if (p.y < value) {
-		color = vec4(114.0/255.0, 156.0/255.0, 172.0/255.0, 1.0);
-	}
-    
-    gl_FragColor = color;
+    gl_FragColor = destColor;
 }
